fe8c156d74007a2abaed3e74da70cf7509bcf31d
Neeraj Sharma <neeraj.sharma@proptiger.com>
2017-05-03 11:55:15 +0530

    updated aws version

--- src/main/java/com/proptiger/hystrix/NameBasedAwsInstanceDiscovery.java
+++ src/main/java/com/proptiger/hystrix/NameBasedAwsInstanceDiscovery.java
@@ -6,10 +6,13 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.netflix.config.DynamicPropertyFactory;
@@ -18,18 +21,27 @@ import com.netflix.turbine.discovery.Instance;
 import com.netflix.turbine.discovery.InstanceDiscovery;
 
 public class NameBasedAwsInstanceDiscovery implements InstanceDiscovery {
+    private static Logger       logger              = LoggerFactory.getLogger(NameBasedAwsInstanceDiscovery.class);
 
-    private DynamicStringProperty INSTANCE_NAME_REGEX =
+    final DynamicStringProperty INSTANCE_NAME_REGEX =
             DynamicPropertyFactory.getInstance().getStringProperty("instance.name.regex", null);
 
-    private Pattern               pattern             = Pattern.compile(INSTANCE_NAME_REGEX.get());
+    final Pattern               pattern;
 
-    private AmazonEC2             ec2                 =
-            AmazonEC2ClientBuilder.standard().withCredentials(DefaultAWSCredentialsProviderChain.getInstance())
-                    .withRegion(Regions.AP_SOUTHEAST_1).build();
+    final AmazonEC2             ec2;
+
+    public NameBasedAwsInstanceDiscovery() {
+        ec2 = new AmazonEC2Client();
+        ec2.setRegion(Region.getRegion(Regions.AP_SOUTHEAST_1));
+
+        pattern = Pattern.compile(INSTANCE_NAME_REGEX.get());
+
+    }
 
     @Override
     public Collection<Instance> getInstanceList() throws Exception {
+
+        logger.info("Started instance discovery");
 
         if (INSTANCE_NAME_REGEX.get() == null) {
             return null;
@@ -50,6 +62,10 @@ public class NameBasedAwsInstanceDiscovery implements InstanceDiscovery {
                 }
             }
         }
+
+        logger.info("Instance list {}", hytrixInstances);
+
+        logger.info("End instance discovery");
 
         return hytrixInstances;
     }
